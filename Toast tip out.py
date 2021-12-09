25a8b2c6f9378ccbf9bc57645bb45bd26a7b8171
Cleu <cleu1@ivytech.edu>
2021-12-09 15:44:20 -0500

    total tipshare calculated

--- Toast tip out.py
+++ Toast tip out.py
@@ -2,12 +2,39 @@
 # Tip out Calc
 
 import csv
+import numpy as np
+infile = 'Shifts_Closed_2021_12_02.csv'
 
 
+def dataIn(infile):
+    dataArray = []
+    with open(infile, newline='') as csv_file:
+        dataArray = list(csv.reader(csv_file))
+        
+    return dataArray
 
-with open('Shifts_Closed_2021_12_02.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-
-    for line in csv_reader:
-        print(line)
+def newList(dataIn):
     
+    dataList = []
+    for column in range (len(dataIn[0])): 
+        for row in range(len(dataIn)):
+            dataList.append(dataIn[row][column])
+    return dataList
+
+
+def tipShareTotal(dataList):
+    total = 0
+    index = dataList.index('Tip Share Total')
+    index +=1
+    for i in range(index, len(dataList)):
+        total = total + float(dataList[i])
+    return total 
+
+
+def main():
+    dataArray = dataIn(infile)
+    dataList = newList(dataArray)
+    totalTipShare = tipShareTotal(dataList)
+    print(totalTipShare)    
+
+main()
